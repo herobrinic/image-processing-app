147a866cee3613979774a6a495248cbbe4713f4a
Ahmed Mohamed Ismail <herobrinegamer2008@gmail.com>
2025-05-15 20:09:51 +0300

    frontend

--- build/index.js
+++ build/index.js
@@ -4,14 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const imageRoutes_1 = __importDefault(require("./routes/imageRoutes"));
 const uploadRoutes_1 = __importDefault(require("./routes/uploadRoutes"));
+const imageRoutes_1 = __importDefault(require("./routes/imageRoutes"));
 const errorHandler_1 = require("./middleware/errorHandler");
 const sanitizeInput_1 = require("./middleware/sanitizeInput");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use(sanitizeInput_1.sanitizeQueryParams); // Sanitize all query params globally
-app.use('/images', imageRoutes_1.default);
-app.use('/upload', uploadRoutes_1.default);
-app.use(errorHandler_1.errorHandler); // Error handler last
-exports.default = app;
+app.use(sanitizeInput_1.sanitizeQueryParams);
+app.use('/api/upload', uploadRoutes_1.default);
+app.use('/api/images', imageRoutes_1.default);
+// Error handling middleware
+app.use(errorHandler_1.errorHandler);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
